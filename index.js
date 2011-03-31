33c3da8cae3671b6e9337053caa7933f14195c36
James Halliday <substack@gmail.com>
2011-03-31 07:23:41 -0700

    now using key event data codes from http://www.aspdotnetfaq.com/Faq/What-is-the-list-of-KeyCodes-for-JavaScript-KeyDown-KeyPress-and-KeyUp-events.aspx

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var data = JSON.parse(fs.readFileSync(
     __dirname + '/data/keysyms.json', 'utf8'
 ));
 
-var shifted = JSON.parse(fs.readFileSync(
-    __dirname + '/data/shifted.json', 'utf8'
+var events = JSON.parse(fs.readFileSync(
+    __dirname + '/data/events.json', 'utf8'
 ));
 
 exports.records = data.records;
@@ -15,27 +15,48 @@ exports.fromKeysym = function (keysym) {
 };
 
 exports.fromUnicode = function (code) {
-    return (data.unicodes[code] || []).map(function (i) {
-        return data.records[i]
-    });
+    if (typeof code === 'string') {
+        if (code.length !== 1) {
+            throw new Error('String must be 1 character');
+        }
+        return exports.fromUnicode(code.charCodeAt(0));
+    }
+    else {
+        return (data.unicodes[code] || []).map(function (i) {
+            return data.records[i]
+        });
+    }
 };
 
 var a = 'a'.charCodeAt(0), z = 'z'.charCodeAt(0);
 var A = 'A'.charCodeAt(0), Z = 'Z'.charCodeAt(0);
 
+function lookup (e) {
+    return e.length === 1
+        ? exports.fromUnicode(e)[0].keysym
+        : exports.fromName(e).keysym
+    ;
+}
+
 exports.keyEvent = function (code, shiftMask) {
     if (a <= code && code <= z) {
-        return code - (shiftMask ? A - a : 0);
+        return code - (shiftMask ? a - A : 0);
     }
     else if (A <= code && code <= Z) {
-        return code + (shiftMask ? 0 : A - a);
+        return code + (shiftMask ? 0 : a - A);
     }
-    else if (shifted[code]) {
-        return String.fromCharCode(shifted[code]);
+    else if (events.both[code]) {
+        return lookup(events.both[code]);
+    }
+    else if (shiftMask && events.shifted[code]) {
+        return lookup(events.shifted[code]);
+    }
+    else if (!shiftMask && events.unshifted[code]) {
+        return lookup(events.unshifted[code]);
     }
     else {
         var c = exports.fromUnicode(code)[0];
-        return c && c.keysym;
+        return c && c.keysym || code;
     }
 };
 
